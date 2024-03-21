a1ec6e84c53126053c50b8571d2a38b0f007327f
Lakshman Karunasena <lakkaru@gmail.com>
2024-03-21 15:34:08 +0530

    feat- frontend react and mui/icons added

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import TopNav from './layouts/common/TopNav';
+
+function App() {
+  return (
+    <div className="App">
+      <TopNav/>
+    </div>
+  );
+}
+
+export default App;
